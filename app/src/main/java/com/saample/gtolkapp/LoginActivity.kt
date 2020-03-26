4aa64a8dc893d9c7051b967248ba5f80187d1361
saa2525 <syunnmai9509>
2020-03-26 11:23:46 +0900

    ドロワーにスピナー実装 ・エミュレーター起動できず ・makeTextエラー

--- app/src/main/java/com/saample/gtolkapp/LoginActivity.kt
+++ app/src/main/java/com/saample/gtolkapp/LoginActivity.kt
@@ -2,12 +2,16 @@ package com.saample.gtolkapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -22,28 +26,82 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginButton.setOnClickListener { v ->
-            // FirebaseAuthのオブジェクトを取得
-            auth = FirebaseAuth.getInstance()
-            Log.d("current", "currentUser = ${auth.currentUser?.uid.toString()}")
-            signInAnonymously()
+        mDataBaseReference = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+
+        mLoginListener = OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // 成功した場合
+                val user = auth.currentUser
+                val userRef = mDataBaseReference.child(UsersPATH).child(user!!.uid)
+
+            } else {
+                // 失敗した場合
+                // エラーを表示する
+                val view = findViewById<View>(android.R.id.content)
+                Snackbar.make(view, "ログインに失敗しました", Snackbar.LENGTH_LONG).show()
+
+                // プログレスバーを非表示にする
+                progressBar.visibility = View.GONE
+            }
+
+            loginButton.setOnClickListener { v ->
+                // FirebaseAuthのオブジェクトを取得
+                Log.d("current", "currentUser = ${auth.currentUser?.uid.toString()}")
+                signInAnonymously()
+
+            }
+            nologinButton.setOnClickListener { v ->
+                Snackbar.make(v, "ログインできませんでした", Snackbar.LENGTH_LONG).show()
+            }
         }
+    }
+
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     //匿名認証リスナー
     private fun signInAnonymously() {
+
         auth.signInAnonymously()
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // 成功した場合, update UI with the signed-in user's information
-                    Log.d("success", "currentUser = ${auth.currentUser?.uid.toString()}")
-
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success")
+                    val user = auth.currentUser
+                    updateUI(user)
                 } else {
-                    // 失敗した場合, display a message to the user.
-                    Log.d("error", task.exception.toString())
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
                 }
+
             }
+
     }
 
-}
+    private fun updateUI(user: FirebaseUser?) {
+        val isSignedIn = user != null
+    }
 
+     fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.loginButton -> signInAnonymously()
+        }
+    }
+
+    companion object {
+        private val TAG = "AnonymousAuth"
+    }
+}
